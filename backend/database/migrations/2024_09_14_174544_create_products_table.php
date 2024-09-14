6fe39dc0a4bb2778b2f2962d9b8926a73b8e0748
zahra_ove <zahra.oveyedzade@gmail.com>
2024-09-14 22:39:43 +0330

    Fix: remove fk in migration file for products becuase of mongodb

--- backend/database/migrations/2024_09_14_174544_create_products_table.php
+++ backend/database/migrations/2024_09_14_174544_create_products_table.php
@@ -8,16 +8,12 @@ return new class extends Migration
 {
     public function up(): void
     {
-        Schema::create('products', function (Blueprint $table) {
-            $table->id();
-            $table->string('name', 255)->unique();
-            $table->decimal('price', 15,2);
-
-            $table->unsignedInteger('inventory_id')->nullable();
-            $table->index('inventory_id', 'product_inventory_indx');
-            $table->foreign('inventory_id', 'product_inventory_fk')->references('id')->on('inventories')->onDelete('set null')->onUpdate('cascade');
-
-            $table->timestamps();
+        Schema::create('products', function ($collection) {
+            $collection->string('name', 255)->unique();
+            $collection->double('price');
+            $collection->string('inventory_id')->nullable();
+            $collection->index('inventory_id', 'product_inventory_indx');
+            $collection->timestamps();
         });
     }
 
